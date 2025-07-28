79eab3c753762251a5e76de7605ec8658c5a3099
anthonycg <anthonygibson24@gmail.com>
2025-07-28 03:03:59 -0500

    setup s3 bucket for img upload

--- src/server/controllers/imgController.ts
+++ src/server/controllers/imgController.ts
@@ -1,12 +1,56 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { IncomingMessage, ServerResponse } from "http";
 
-export function uploadImage(req: IncomingMessage, res: ServerResponse) {
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "image/jpeg");
-    res.end("Image link");
+export async function uploadImage(req: IncomingMessage, res: ServerResponse) {
+    // init s3 Client
+    const s3Client = new S3Client({
+        region: process.env.AWS_REGION,
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+        },
+    });
+    // await buffer objs
+    const streams: Buffer[] = [];
+    // push new data to streams arr
+    req.on("data", (stream) => streams.push(stream));
+    // combine all data
+    req.on("end", async () => {
+        const buffer = Buffer.concat(streams);
+        const contentType =
+            req.headers["content-type"] || "application/octet-stream";
+
+        // get fileName
+        const fileName = req.headers["x-file-name"] as string | undefined;
+        if (!fileName) {
+            res.statusCode = 400;
+            res.end("error: Missing file name");
+            return;
+        }
+        const key = `uploads/${fileName}`;
+
+        try {
+            await s3Client.send(
+                new PutObjectCommand({
+                    Bucket: process.env.S3_BUCKET_NAME,
+                    Key: key,
+                    Body: buffer,
+                    ContentType: contentType,
+                })
+            );
+
+            const url = `https://${process.env.S3_BUCKET_NAME}.s3.amazonaws.com/${key}`;
+            res.writeHead(200, { "Content-Type": "application/json" });
+            res.end(JSON.stringify({ message: "Upload successful", url }));
+        } catch (error) {
+            console.log("Upload failed:::", error);
+            res.statusCode = 500;
+            res.end("Error: Upload Failed!");
+        }
+    });
 }
 
-export function getImages(req: IncomingMessage, res: ServerResponse) {
+export async function getImages(req: IncomingMessage, res: ServerResponse) {
     res.statusCode = 200;
     res.setHeader("Content-Type", "image/jpeg");
     res.end("Image link");
